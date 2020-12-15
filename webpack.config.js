65a599f5aa9e3fd699cbf2ea4972109e16374f93
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2020-12-15 16:22:03 +0900

    Installed css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
   }
 }
