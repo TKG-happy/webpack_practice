510bebf92394f7c256accaaeac2c17e5e0d3f5af
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2020-12-18 07:20:27 +0900

    Installed Typescript

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,7 @@ import './reactApp.jsx';
 import Vue from 'vue';
 import VueApp from './VueApp.vue';
 import my from './my.js';
+import add from './add.ts';
 import '../stylesheets/main.scss';
 
 new Vue({
@@ -9,5 +10,6 @@ new Vue({
   render: (h) => h(VueApp),
 });
 
+console.log(add(3, 9));
 console.log('webpack');
 my();
