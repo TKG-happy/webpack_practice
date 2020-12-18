37d730d9b9b59326cfe07e6ffffac6e1331cfbcf
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2020-12-19 03:41:48 +0900

    Clean up project

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,15 +1,2 @@
 import './reactApp.jsx';
-import Vue from 'vue';
-import VueApp from './VueApp.vue';
-import my from './my.js';
-import add from './add.ts';
 import '../stylesheets/main.scss';
-
-new Vue({
-  el: '#vue-root',
-  render: (h) => h(VueApp),
-});
-
-console.log(add(3, 9));
-console.log('webpack');
-my();
